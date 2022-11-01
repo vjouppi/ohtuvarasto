c46c3452408a94c9bdda6b60d3dad5ca3a4f64ef
Ville Jouppi <jope@jope.fi>
2022-11-01 19:48:20 +0200

    tests/varasto_test: paranna varaston coverity sataan prosenttiin

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,39 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_lisays_palauttaa(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_voi_lisata_liikaa(self):
+        self.varasto.lisaa_varastoon(100)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_ei_voi_ottaa_neg(self):
+        self.varasto.lisaa_varastoon(1)
+        self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 1)
+
+    def test_otetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.ota_varastosta(6)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_palautetaan_string(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+        
+    def test_tilavuus_negsize(self):
+        self.varasto_negsize = Varasto(-1)
+
+        self.assertAlmostEqual(self.varasto_negsize.tilavuus, 0)
+
+    def test_alkusaldo_neg(self):
+        self.varasto_negsaldo = Varasto(10,-1)
+
+        self.assertAlmostEqual(self.varasto_negsaldo.saldo, 0)
+
