bfcb2054e2f11c0a9ef91cb331315d2d70859cc8
Ville Jouppi <jope@jope.fi>
2022-11-14 16:59:40 +0200

    lint kuntoon

--- src/varasto.py
+++ src/varasto.py
@@ -1,23 +1,8 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
-#        self.tilavuus = max(tilavuus, 0.0)
-#        self.saldo = max(alku_saldo, 0.0)
-#        self.saldo = min(self.saldo, self.tilavuus)
+        self.tilavuus = max(tilavuus, 0.0)
+        self.saldo = max(alku_saldo, 0.0)
+        self.saldo = min(self.saldo, self.tilavuus)
 
     # huom: ominaisuus voidaan myös laskea.
     # Ei tarvita erillistä kenttää viela_tilaa tms.
